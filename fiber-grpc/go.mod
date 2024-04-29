17fee615958a5496c11e79f9e7ec2b653b049573
ovaphlow <ovaphlow@live.com>
2024-04-29 14:10:35 +0800

    ddd update remove

--- fiber-grpc/go.mod
+++ fiber-grpc/go.mod
@@ -4,7 +4,7 @@ go 1.21.6
 
 require (
 	github.com/bwmarrin/snowflake v0.3.0
-	github.com/gofiber/fiber/v2 v2.52.1
+	github.com/gofiber/fiber/v2 v2.52.4
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
@@ -14,18 +14,39 @@ require (
 )
 
 require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/PuerkitoBio/purell v1.2.1 // indirect
+	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
+	github.com/go-openapi/jsonpointer v0.21.0 // indirect
+	github.com/go-openapi/jsonreference v0.21.0 // indirect
+	github.com/go-openapi/spec v0.21.0 // indirect
+	github.com/go-openapi/swag v0.23.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/klauspost/compress v1.17.7 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/klauspost/compress v1.17.8 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/swaggo/fiber-swagger v1.3.0 // indirect
+	github.com/swaggo/files v1.0.1 // indirect
+	github.com/swaggo/swag v1.16.3 // indirect
+	github.com/urfave/cli/v2 v2.27.2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.52.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/net v0.22.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20240312152122-5f08fbb34913 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/tools v0.20.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240304212257-790db918fca8 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	sigs.k8s.io/yaml v1.4.0 // indirect
 )
--- fiber-grpc/schema.go
+++ fiber-grpc/schema.go
@@ -6,6 +6,8 @@ import (
 	"errors"
 	"fmt"
 	"ovaphlow/cratecyclone/models"
+	"ovaphlow/cratecyclone/utility"
+	"strconv"
 	"strings"
 	"time"
 
@@ -23,7 +25,10 @@ type SchemaRepo interface {
 	listSchemas() ([]string, error)
 	listTables(schema string) ([]string, error)
 	listColumns(schema, table string) ([]Column, error)
-	save(schema *string, table *string, data map[string]interface{}) error
+	save(schema, table string, data map[string]interface{}) error
+	get(schema, table string, id int64, uuid string) (map[string]interface{}, error)
+	update(schema, table string, id int64, uuid string, data map[string]interface{}) error
+	remove(schema, table string, id int64, uuid string) error
 }
 
 type SchemaRepoImpl struct {
@@ -96,7 +101,7 @@ func (r *SchemaRepoImpl) listColumns(schema, table string) ([]Column, error) {
 	return columns, nil
 }
 
-func (r *SchemaRepoImpl) save(schema *string, table *string, data map[string]interface{}) error {
+func (r *SchemaRepoImpl) save(schema string, table string, data map[string]interface{}) error {
 	columns := []string{}
 	flags := []string{}
 	params := []interface{}{}
@@ -106,8 +111,79 @@ func (r *SchemaRepoImpl) save(schema *string, table *string, data map[string]int
 		params = append(params, data[column])
 	}
 	_, err := r.db.Exec(
-		fmt.Sprintf("insert into %s.%s (%s) values (%s)", *schema, *table, strings.Join(columns, ", "), strings.Join(flags, ", ")),
+		fmt.Sprintf("insert into %s.%s (%s) values (%s)", schema, table, strings.Join(columns, ", "), strings.Join(flags, ", ")),
 		params...,
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *SchemaRepoImpl) get(schema, table string, id int64, uuid string) (map[string]interface{}, error) {
+	data := make(map[string]interface{})
+	rows, err := r.db.Query(
+		fmt.Sprintf("select * from %s.%s where id = $1 and state ->> 'uuid' = $2", schema, table),
+		id,
+		uuid,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	if rows.Next() {
+		columns, err := rows.Columns()
+		if err != nil {
+			return nil, err
+		}
+		values := make([]interface{}, len(columns))
+		valuePtrs := make([]interface{}, len(columns))
+		for i := range columns {
+			valuePtrs[i] = &values[i]
+		}
+		err = rows.Scan(valuePtrs...)
+		if err != nil {
+			return nil, err
+		}
+		for i, col := range columns {
+			val := values[i]
+			b, ok := val.([]byte)
+			if ok {
+				data[col] = string(b)
+			} else {
+				data[col] = val
+			}
+		}
+	}
+	return data, nil
+}
+
+func (r *SchemaRepoImpl) update(schema, table string, id int64, uuid string, data map[string]interface{}) error {
+	columns := []string{}
+	params := []interface{}{}
+	for column := range data {
+		if data[column] == nil {
+			columns = append(columns, column)
+		} else {
+			columns = append(columns, fmt.Sprintf("%s = $%d", column, len(columns)+1))
+			params = append(params, data[column])
+		}
+	}
+	_, err := r.db.Exec(
+		fmt.Sprintf("update %s.%s set %s where id = $%d and state ->> 'uuid' = $%d", schema, table, strings.Join(columns, ", "), len(params)+1, len(params)+2),
+		append(params, id, uuid)...,
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *SchemaRepoImpl) remove(schema, table string, id int64, uuid string) error {
+	_, err := r.db.Exec(
+		fmt.Sprintf("update %s.%s set state = state || jsonb_build_object('deleted_at', '%s') where id = $1 and state ->> 'uuid' = $2", schema, table, time.Now().Format("2006-01-02 15:04:05")),
+		id,
+		uuid,
 	)
 	if err != nil {
 		return err
@@ -135,8 +211,8 @@ func (s *SchemaService) ListColumns(schema, table string) ([]Column, error) {
 	return s.repo.listColumns(schema, table)
 }
 
-func (s *SchemaService) Save(schema *string, table *string, data map[string]interface{}) error {
-	columns, err := s.ListColumns(*schema, *table)
+func (s *SchemaService) Save(schema string, table string, data map[string]interface{}) error {
+	columns, err := s.ListColumns(schema, table)
 	if err != nil {
 		return err
 	}
@@ -175,15 +251,32 @@ func (s *SchemaService) Save(schema *string, table *string, data map[string]inte
 		}
 	}
 	if !validateData {
-		return errors.New("数据不完整")
+		return errors.New("数据不匹配")
 	}
 	return s.repo.save(schema, table, data)
+}
+
+func (s *SchemaService) Get(schema, table string, id int64, uuid string) (map[string]interface{}, error) {
+	return s.repo.get(schema, table, id, uuid)
+}
+
+func (s *SchemaService) Update(schema, table string, id int64, uuid string, data map[string]interface{}) error {
+	data[fmt.Sprintf(
+		"state = state || jsonb_build_object('updated_at', '%s')",
+		time.Now().Format("2006-01-02 15:04:05"),
+	)] = nil
+	return s.repo.update(schema, table, id, uuid, data)
+}
+
+func (s *SchemaService) Remove(schema, table string, id int64, uuid string) error {
+	return s.repo.remove(schema, table, id, uuid)
 }
 
 func AddSchemaEndpoints(app *fiber.App, service *SchemaService) {
 	app.Get("/crate-api/db-schema", func(c *fiber.Ctx) error {
 		schemas, err := service.ListSchemas()
 		if err != nil {
+			utility.Slogger.Error(err.Error())
 			return c.Status(500).JSON(models.ErrorResponse{
 				Type:     "about:blank",
 				Status:   500,
@@ -199,6 +292,7 @@ func AddSchemaEndpoints(app *fiber.App, service *SchemaService) {
 		schema := c.Params("schema")
 		tables, err := service.ListTables(schema)
 		if err != nil {
+			utility.Slogger.Error(err.Error())
 			return c.Status(500).JSON(models.ErrorResponse{
 				Type:     "about:blank",
 				Status:   500,
@@ -215,6 +309,7 @@ func AddSchemaEndpoints(app *fiber.App, service *SchemaService) {
 		table := c.Params("table")
 		columns, err := service.ListColumns(schema, table)
 		if err != nil {
+			utility.Slogger.Error(err.Error())
 			return c.Status(500).JSON(models.ErrorResponse{
 				Type:     "about:blank",
 				Status:   500,
@@ -231,6 +326,7 @@ func AddSchemaEndpoints(app *fiber.App, service *SchemaService) {
 		table := c.Params("table")
 		data := make(map[string]interface{})
 		if err := c.BodyParser(&data); err != nil {
+			utility.Slogger.Error(err.Error())
 			return c.Status(400).JSON(models.ErrorResponse{
 				Type:     "about:blank",
 				Status:   400,
@@ -239,8 +335,9 @@ func AddSchemaEndpoints(app *fiber.App, service *SchemaService) {
 				Instance: c.OriginalURL(),
 			})
 		}
-		err := service.Save(&schema, &table, data)
+		err := service.Save(schema, table, data)
 		if err != nil {
+			utility.Slogger.Error(err.Error())
 			return c.Status(500).JSON(models.ErrorResponse{
 				Type:     "about:blank",
 				Status:   500,
@@ -250,5 +347,108 @@ func AddSchemaEndpoints(app *fiber.App, service *SchemaService) {
 			})
 		}
 		return c.Status(201).JSON(data)
+	})
+
+	app.Get("/crate-api/:schema/:table/:id", func(c *fiber.Ctx) error {
+		schema := c.Params("schema", "")
+		table := c.Params("table", "")
+		id, err := strconv.ParseInt(c.Params("id", "0"), 10, 64)
+		if err != nil {
+			utility.Slogger.Error(err.Error())
+			return c.Status(400).JSON(models.ErrorResponse{
+				Type:     "about:blank",
+				Status:   400,
+				Title:    "参数错误",
+				Detail:   err.Error(),
+				Instance: c.OriginalURL(),
+			})
+		}
+		uuid := c.Query("uuid", "")
+		data, err := service.Get(schema, table, id, uuid)
+		if err != nil {
+			utility.Slogger.Error(err.Error())
+			return c.Status(500).JSON(models.ErrorResponse{
+				Type:     "about:blank",
+				Status:   500,
+				Title:    "服务器错误",
+				Detail:   err.Error(),
+				Instance: c.OriginalURL(),
+			})
+		}
+		for key := range data {
+			if value, ok := data[key].(int64); ok {
+				data["_"+key] = strconv.FormatInt(value, 10)
+			}
+		}
+		return c.JSON(data)
+	})
+
+	app.Put("/crate-api/:schema/:table/:id", func(c *fiber.Ctx) error {
+		schema := c.Params("schema", "")
+		table := c.Params("table", "")
+		id, err := strconv.ParseInt(c.Params("id", "0"), 10, 64)
+		if err != nil {
+			utility.Slogger.Error(err.Error())
+			return c.Status(400).JSON(models.ErrorResponse{
+				Type:     "about:blank",
+				Status:   400,
+				Title:    "参数错误",
+				Detail:   err.Error(),
+				Instance: c.OriginalURL(),
+			})
+		}
+		uuid := c.Query("uuid", "")
+		data := make(map[string]interface{})
+		if err := c.BodyParser(&data); err != nil {
+			utility.Slogger.Error(err.Error())
+			return c.Status(400).JSON(models.ErrorResponse{
+				Type:     "about:blank",
+				Status:   400,
+				Title:    "参数错误",
+				Detail:   err.Error(),
+				Instance: c.OriginalURL(),
+			})
+		}
+		err = service.Update(schema, table, id, uuid, data)
+		if err != nil {
+			utility.Slogger.Error(err.Error())
+			return c.Status(500).JSON(models.ErrorResponse{
+				Type:     "about:blank",
+				Status:   500,
+				Title:    "服务器错误",
+				Detail:   err.Error(),
+				Instance: c.OriginalURL(),
+			})
+		}
+		return c.SendStatus(200)
+	})
+
+	app.Delete("/crate-api/:schema/:table/:id", func(c *fiber.Ctx) error {
+		schema := c.Params("schema", "")
+		table := c.Params("table", "")
+		id, err := strconv.ParseInt(c.Params("id", "0"), 10, 64)
+		if err != nil {
+			utility.Slogger.Error(err.Error())
+			return c.Status(400).JSON(models.ErrorResponse{
+				Type:     "about:blank",
+				Status:   400,
+				Title:    "参数错误",
+				Detail:   err.Error(),
+				Instance: c.OriginalURL(),
+			})
+		}
+		uuid := c.Query("uuid", "")
+		err = service.Remove(schema, table, id, uuid)
+		if err != nil {
+			utility.Slogger.Error(err.Error())
+			return c.Status(500).JSON(models.ErrorResponse{
+				Type:     "about:blank",
+				Status:   500,
+				Title:    "服务器错误",
+				Detail:   err.Error(),
+				Instance: c.OriginalURL(),
+			})
+		}
+		return c.SendStatus(204)
 	})
 }
