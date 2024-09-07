b4e49a36357154b7d6f38927cae28763e76b881a
ovaphlow <ovaphlow@live.com>
2024-09-07 13:02:04 +0800

    remove gin/

--- gin/infrastruction/models.go
+++
@@ -1,9 +0,0 @@
-package infrastruction
-
-type ErrorResponse struct {
-	Type     string `json:"type"`
-	Status   int    `json:"status"`
-	Title    string `json:"title"`
-	Detail   string `json:"detail"`
-	Instance string `json:"instance"`
-}
