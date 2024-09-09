ffb738ffacae2ee8ed9e48393d6ab48adc326e16
ovaphlow <ovaphlow@live.com>
2024-09-09 21:49:31 +0800

    代理

--- hono-bun-region-spec/src/index.ts
+++ hono-bun-region-spec/src/index.ts
@@ -1,4 +1,5 @@
 import { serve } from '@hono/node-server'
+import axios from 'axios'
 import { Hono } from 'hono'
 import * as dotenv from 'dotenv'
 import weixinZhCnRouter from './routes/weixin-zhcn'
@@ -6,10 +7,45 @@ import emailZhCnRouter from './routes/email-zhcn'
 
 dotenv.config()
 
+const initHQ = async () => {
+    const addr = process.env.HQ_ADDR || '127.0.0.1'
+    const port = process.env.HQ_PORT || 8421
+    const body = {
+        name: 'crate-region-spec-api',
+        protocol: 'http',
+        host: '127.0.0.1',
+        port: 8488,
+        healthCheck: {
+            endpoint: '/healthCheck'
+        }
+    }
+    try {
+        const response = await axios.post(`http://${addr}:${port}/crate-hq-api/service`, body)
+        if (response.status < 400) {
+            console.info('initHQ success')
+        }
+    } catch (error) {
+        console.error(error)
+    }
+}
+initHQ()
+
 const app = new Hono()
+
+app.use('*', async (c, next) => {
+    const { method, url } = c.req
+    const start = Date.now()
+    await next()
+    const ms = Date.now() - start
+    console.log(`${method} ${url} - ${ms}ms`)
+})
 
 app.get('/', (c) => {
     return c.text('Hello Hono!')
+})
+
+app.get('/healthCheck', (c) => {
+    return c.text('OK')
 })
 
 app.route('/crate-region-spec-api/zhcn/weixin', weixinZhCnRouter)
--- hono-bun-region-spec/src/routes/email-zhcn.ts
+++ hono-bun-region-spec/src/routes/email-zhcn.ts
@@ -3,6 +3,7 @@ import { Hono } from 'hono'
 const router = new Hono()
 
 router.get('/example', (c) => {
+    console.info(c.req.method, c.req.path)
     return c.text('Hello, Hono!')
 })
 
