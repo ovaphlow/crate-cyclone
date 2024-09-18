28ad662e1348a2aa788a8d52af97489c3c277852
ovaphlow <ovaphlow@live.com>
2024-09-18 15:37:31 +0800

    echo

---
+++ hyper-cyclone/src/main.rs
@@ -0,0 +1,104 @@
+// use std::convert::Infallible;
+use std::net::SocketAddr;
+
+use http_body_util::{combinators::BoxBody, BodyExt};
+use http_body_util::{Empty, Full};
+use hyper::body::{Body, Bytes, Frame};
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Method, Request, Response, StatusCode};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
+
+async fn echo(
+    req: Request<hyper::body::Incoming>,
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => Ok(Response::new(full("Try POSTing data to /echo"))),
+        (&Method::POST, "/echo") => Ok(Response::new(req.into_body().boxed())),
+        (&Method::POST, "/echo/uppercase") => {
+            // Map this body's frame to a different type
+            let frame_stream = req.into_body().map_frame(|frame| {
+                let frame = if let Ok(data) = frame.into_data() {
+                    // Convert every byte in every Data frame to uppercase
+                    data.iter()
+                        .map(|byte| byte.to_ascii_uppercase())
+                        .collect::<Bytes>()
+                } else {
+                    Bytes::new()
+                };
+
+                Frame::data(frame)
+            });
+            Ok(Response::new(frame_stream.boxed()))
+        }
+        (&Method::POST, "/echo/reversed") => {
+            // Protect our server from massive bodies.
+            let upper = req.body().size_hint().upper().unwrap_or(u64::MAX);
+            if upper > 1024 * 64 {
+                let mut resp = Response::new(full("Body too big"));
+                *resp.status_mut() = hyper::StatusCode::PAYLOAD_TOO_LARGE;
+                return Ok(resp);
+            }
+
+            // Await the whole body to be collected into a single `Bytes`...
+            let whole_body = req.collect().await?.to_bytes();
+
+            // Iterate the whole body in reverse order and collect into a new Vec.
+            let reversed_body = whole_body.iter().rev().cloned().collect::<Vec<u8>>();
+
+            Ok(Response::new(full(reversed_body)))
+        }
+
+        // Return 404 Not Found for other routes.
+        _ => {
+            let mut not_found = Response::new(empty());
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
+}
+
+// We create some utility functions to make Empty and Full bodies
+// fit our broadened Response body type.
+fn empty() -> BoxBody<Bytes, hyper::Error> {
+    Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
+}
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
+}
+
+// async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+//     Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+// }
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8448));
+
+    let listener = TcpListener::bind(addr).await?;
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        // Use an adapter to access something implementing `tokio::io` traits as if they implement
+        // `hyper::rt` IO traits.
+        let io = TokioIo::new(stream);
+
+        // Spawn a tokio task to serve multiple connections concurrently
+        tokio::task::spawn(async move {
+            // Finally, we bind the incoming connection to our `hello` service
+            if let Err(err) = http1::Builder::new()
+                // `service_fn` converts our function in a `Service`
+                .serve_connection(io, service_fn(echo))
+                .await
+            {
+                eprintln!("Error serving connection: {:?}", err);
+            }
+        });
+    }
+}
